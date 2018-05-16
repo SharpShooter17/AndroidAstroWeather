591ac5af2f552c6a25ee2ebe1894b8a9875cbede
SharpShooter <b.ujazdowski@gmail.com>
2018-05-16 13:20:26 +0200

    przypisanie pol

--- app/src/main/java/com/bartoszujazdowski/astroweather/MoreInfoFragment.java
+++ app/src/main/java/com/bartoszujazdowski/astroweather/MoreInfoFragment.java
@@ -6,13 +6,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MoreInfoFragment extends Fragment {
+
+    private TextView windForceTV;
+    private TextView windDirectionTV;
+    private TextView humidityTV;
+    private TextView visabilityTV;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstance){
         View view = inflater.inflate(R.layout.more_info_fragment, container, false);
+
+        this.windForceTV = view.findViewById(R.id.windForceTextView);
+        this.windDirectionTV = view.findViewById(R.id.windDirectionTextView);
+        this.humidityTV = view.findViewById(R.id.humidityTextView);
+        this.visabilityTV = view.findViewById(R.id.visabilityTextView);
+
         return view;
     }
 
--- app/src/main/java/com/bartoszujazdowski/astroweather/WeatherFragment.java
+++ app/src/main/java/com/bartoszujazdowski/astroweather/WeatherFragment.java
@@ -6,13 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class WeatherFragment extends Fragment {
+
+    private TextView cityTV ;
+    private TextView temperatureTV;
+    private TextView pressureTV;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstance){
         View view = inflater.inflate(R.layout.weather_fragment, container, false);
+
+        this.cityTV = (TextView) view.findViewById(R.id.cityTextView);
+        this.temperatureTV = (TextView) view.findViewById(R.id.temperatureTextView);
+        this.pressureTV = (TextView) view.findViewById(R.id.pressureTextView);
+
         return view;
     }
 
